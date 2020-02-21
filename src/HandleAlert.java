447b0adcea962c26e56d2694a5e77baccc829c92
abhishek.vishnoi <abhishek.vishnoi@USABOS146046L.ad.infosys.com>
2020-02-21 00:18:32 -0600

    Adding first file in git repo

---
+++ src/HandleAlert.java
@@ -0,0 +1,26 @@
+package seleniumSession;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleAlert {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","D:\\chromedriver78\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		
+		
+		driver.findElement(By.name("proceed")).click();
+		Alert alert = 	driver.switchTo().alert();
+		alert.getText();
+		System.out.println(alert.getText());
+		alert.accept();
+		//alert.dismiss();
+		
+	}
+
+}
